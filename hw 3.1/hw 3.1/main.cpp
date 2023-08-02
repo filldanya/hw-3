a1e753d2f282ae4feec790abf78a103daede4676
Daniil <daniilfilichew@gmail.com>
2023-08-02 19:07:52 +0300

    second commit

--- hw 3.1/hw 3.1/main.cpp
+++ hw 3.1/hw 3.1/main.cpp
@@ -53,16 +53,23 @@ int main() {
 	std::cout << "Введите num2: ";
 	std::cin >> num2;
 	math.set_num2(num2);
-	if (math.set_num1(num1) && math.set_num2(num2)) {
+	if (!math.set_num1(num1) ) {
+		std::cout << "Неверный ввод" << std::endl << "Введите num1:";
+		std::cin >> num1;
+		math.set_num1(num1);
+	}
+	else if (!math.set_num2(num2)) {
+		std::cout << "Неверный ввод" << std::endl << "Введите num2:";
+		std::cin >> num2;
+		math.set_num2(num2);
+	}
+	
 		std::cout << "num1 + num2 = " << math.add() << std::endl;
 		std::cout << "num1 * num2 = " << math.multiply() << std::endl;
 		std::cout << "num2 - num1 = " << math.subtract_1_2() << std::endl;
 		std::cout << "num1 - num2 = " << math.subtract_2_1() << std::endl;
 		std::cout << "num1 / num2 = " << math.divide_1_2() << std::endl;
 		std::cout << "num2 / num1 = " << math.divide_2_1() << std::endl;
-	}
-	else {
-		std::cout << "Неверный ввод:" << std::endl;
-	}
+	
 }
 }
